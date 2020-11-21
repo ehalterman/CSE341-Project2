930040c692b02f135cd211690f4b52706d9ae8bb
ehalterman <hal17034@byui.edu>
2020-11-20 23:15:38 -0700

    add app.get

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + '/public'));
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
-//app.get('/newBaby', newBaby);
+app.get('/newBaby', newBaby);
 
 // // start the server listening
 app.listen(app.get('port'), function () {
