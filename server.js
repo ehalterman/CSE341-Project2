843889a07397ef676a52c065ea2e9ab3d0d7d8e3
ehalterman <hal17034@byui.edu>
2020-11-20 23:36:17 -0700

    quotes in sql

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ function newBaby(req, response) {
     function handleBaby (first_name, last_name, dob, callback) {
       console.log("inserting baby: " + first_name);
       
-      const sql = `INSERT INTO Baby (first_name, last_name, dob) VALUES ( "${first_name}", "${last_name}", ${dob}`;
+      const sql = `INSERT INTO Baby (first_name, last_name, dob) VALUES ( '${first_name}', '${last_name}', '${dob}'`;
       pool.query(sql, function (err, result) {
           if (err) {
             console.log("Error in query: ");
