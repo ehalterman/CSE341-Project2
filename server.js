b234f20988e4bb07c8d6430c9e9a1286fcbdedb3
ehalterman <hal17034@byui.edu>
2020-12-03 22:41:36 -0700

    new post method

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express();
 const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL;
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true,})
+)
 
 const pool = new Pool({
   connectionString: connectionString
@@ -23,38 +25,48 @@ app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 //
-function newBaby(req, response) {
-  const first_name = req.body.first_name;
-  const last_name = req.body.last_name;
-  const dob = Date(req.body.dob);
+const newBaby = (request, response) => {
+  const {first_name, last_name, dob} = request.body
 
-  handleBaby(first_name, last_name, dob, function (error, result) {
-      // This is the callback function that will be called when the DB is done.
-      // The job here is just to send it back.
+  pool.query ('INSERT INTO Baby (first_name, last_name, dob) VALUES ( $1, $2, $3)', [first_name, last_name, dob], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`Baby added with ID: $result.insertId}`)
+  })
+}
 
-      // Make sure we got a row with the person, then prepare JSON to send back
-      if (error || result == null || result.length != 1) {
-        response.status(500).json({
-          success: false,
-          data: error
-        });
-      } else {
-        const baby = result[0];
-        response.status(200).json(baby);
-      }
-    });
-  }
 
-    function handleBaby (first_name, last_name, dob, callback) {
-      console.log("inserting baby: " + first_name);
+// function newBaby(req, response) {
+//   const input {first_name, last_name, dob} = req.body
+
+//   handleBaby(first_name, last_name, dob, function (error, result) {
+//       // This is the callback function that will be called when the DB is done.
+//       // The job here is just to send it back.
+
+//       // Make sure we got a row with the person, then prepare JSON to send back
+//       if (error || result == null || result.length != 1) {
+//         response.status(500).json({
+//           success: false,
+//           data: error
+//         });
+//       } else {
+//         const baby = result[0];
+//         response.status(200).json(baby);
+//       }
+//     });
+//   }
+
+//     function handleBaby (first_name, last_name, dob, callback) {
+//       console.log("inserting baby: " + first_name);
       
-      const sql = `INSERT INTO Baby (first_name, last_name, dob) VALUES ( '${first_name}', '${last_name}', '${dob}')`;
-      pool.query(sql, function (err, result) {
-          if (err) {
-            console.log("Error in query: ");
-            console.log(err);
-          }
-          console.log("Found Result: " + JSON.stringify(result.rows));
-          callback(null, result.rows);
-        });
-      }
+//       const sql = `INSERT INTO Baby (first_name, last_name, dob) VALUES ( '${first_name}', '${last_name}', '${dob}')`;
+//       pool.query(sql, function (err, result) {
+//           if (err) {
+//             console.log("Error in query: ");
+//             console.log(err);
+//           }
+//           console.log("Found Result: " + JSON.stringify(result.rows));
+//           callback(null, result.rows);
+//         });
+      //}
