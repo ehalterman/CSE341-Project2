734abc816f0bc570b8c82a73753f880c655aeaac
ehalterman <hal17034@byui.edu>
2020-12-03 23:01:31 -0700

    another attempt

--- server.js
+++ server.js
@@ -2,27 +2,26 @@ const express = require('express')
 const app = express();
 const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL;
-app.use(express.json())
-app.use(express.urlencoded({extended: true,})
-)
 
 const pool = new Pool({
   connectionString: connectionString
 });
 
-app.set('port', process.env.PORT);
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.json())
+app.use(express.urlencoded({extended: true,})
+)
 
-// // start the server listening
-app.listen(app.post('port'), function () {
-  console.log('Node app is running on port', app.post('port'));
-});
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 app.post('/newBaby', newBaby);
-
+// // start the server listening
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
 //
